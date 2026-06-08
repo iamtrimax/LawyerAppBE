52dbfd78a78f9e310c03acab80a3e30061c548fe
iamtrimax <trimax2k3@gmail.com>
2026-06-08 16:10:37 +0700

    fix: Increase express JSON payload limit to 50mb

--- src/app.js
+++ src/app.js
@@ -23,7 +23,8 @@ process.on('SIGINT', async () => {
 
 
 app.use(cors());
-app.use(express.json()); // Cho phép nhận JSON body
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Khởi chạy các tiến trình chạy ngầm
 initReminderCron();
