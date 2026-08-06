bbc8b8e792958c379679f3c85b1ce36d98213505
iamtrimax <trimax2k3@gmail.com>
2026-08-06 17:48:47 +0700

    fix feature login with google by role lawyer 3

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -17,7 +17,8 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: false,
+        default: ""
     },
     role: {
         type: String,
